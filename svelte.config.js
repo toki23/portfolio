3f8cc3d6f8ae5b06017dc03f7295b45eac73636f
toki23 <blancshio@gmail.com>
2021-10-16 00:15:19 +0900

    SPAとしてビルドするよう変更

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		files: {
-			lib: "src/lib"
-		}
+			lib: "src/lib",
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		ssr: false,
 	}
 };
 
